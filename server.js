6491262aa0b314f812d41f5db030c65520640aa7
Anthony Curtis <tonydc1997@gmail.com>
2018-07-14 08:27:13 -0700

    Brainstorm api ideas

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
+
+/*
+/ --> res = this is working
+/signIn --> POST = success / fail
+/register --> POST = user
+/profile/:userID --> GET = user
+/image --> PUT --> user
+
+*/

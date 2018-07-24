3be14039f370a122c441fa543d40a7f3ae10d0f8
Anthony Curtis <tonydc1997@gmail.com>
2018-07-24 08:29:14 -0700

    Remove redundant / inefficient code

--- server.js
+++ server.js
@@ -82,17 +82,7 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  })
-  if (!found) {
-    res.status(400).json('Sorry, not found!');
-  }
+ 
 })
 
 app.listen(3000, () => {

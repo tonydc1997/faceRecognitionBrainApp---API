212420013d44bc0814b3386813ee973ac17dc2c9
Anthony Curtis <tonydc1997@gmail.com>
2018-07-25 08:47:50 -0700

    Add some more logic and catch(err's) to signIn endpoint

--- server.js
+++ server.js
@@ -51,12 +51,17 @@ app.post('/signIn', (req, res) => {
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash); 
       if (isValid) {
-        db.select('*').from('users')
+        return db.select('*').from('users')
           .where('email', '=', email)
-          .then()
-        res.json()
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("Unable to get user"))
+      } else {
+        res.status(400).json("Wrong credentials");
       }
     })
+    .catch(err => console.log(err)) 
 })
 
 app.post('/register', (req, res) => {

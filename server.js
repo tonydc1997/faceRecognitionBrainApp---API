0f638b94bf604b6b340b3360ab8cedcd0744d863
Anthony Curtis <tonydc1997@gmail.com>
2018-07-26 08:21:20 -0700

    Move signIn logic to its component

--- server.js
+++ server.js
@@ -25,25 +25,7 @@ app.get('/', (req, res) => {
   res.send(database.users);
 })
 
-app.post('/signIn', (req, res) => {
-  const { email, password } = req.body;
-  db.select('email', 'hash').from('login')
-    .where('email', '=', email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(password, data[0].hash); 
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', email)
-          .then(user => {
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json("Unable to get user"))
-      } else {
-        res.status(400).json("Wrong credentials");
-      }
-    })
-    .catch(err => res.status(400).json("Wrong credentials")) 
-})
+app.post('/signIn', )
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 

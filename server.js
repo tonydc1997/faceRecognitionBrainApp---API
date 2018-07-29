08fdb37d2ef31adade9301c33d6eae8139dc366a
Anthony Curtis <tonydc1997@gmail.com>
2018-07-29 08:05:03 -0700

    Make node server more dynamic with port

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const db = knex({
 });
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -30,8 +31,8 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.listen(PORT || 3000, () => {
+  console.log(`app is running on ${PORT}`);
 });
 
 /*

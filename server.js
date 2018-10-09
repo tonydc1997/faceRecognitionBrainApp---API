357571cab6a2819ac1f4035f6afaba6eee79d72d
Anthony Curtis <tonydc1997@gmail.com>
2018-10-09 10:13:00 -0700

    Connect backend to docker

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: true,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
   }
 });
 

ad91058175082a4b078fbcc69dfb2d4e32cb95ce
Anthony Curtis <tonydc1997@gmail.com>
2018-10-10 12:26:33 -0700

    Add additional statement and connect to Dockerfile

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,5 +1,6 @@
 BEGIN TRANSACTION;
 
 INSERT INTO users (name, email, entries, joined) VALUES ('Nick', 'nick@gmail.com', 2, '2018-10-05');
+INSERT INTO login (hash, email) VALUES ('$2a$10$urBS/W0noUxD5bgt2ENHV.HyB1FoH4ZB8OPeHTjaDp0HMSHFXYW7K', 'nick@gmail.com');
 
 COMMIT;

b1fb854555665472069b0a075b78eac3d5c791d9
Anthony Curtis <tonydc1997@gmail.com>
2018-10-24 06:40:54 -0700

    feat: Implement authorization middleware on sensitive endpoints

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ app.use(cors());
 app.get('/', (req, res) => { res.send('It is working!') })
 app.post('/signIn', (req, res) => { signIn.signInAuthentication(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db) })
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db) })
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageUrl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res) })
 app.listen(PORT || 3000, () => {
   console.log(`app is running on ${PORT}`);
 });

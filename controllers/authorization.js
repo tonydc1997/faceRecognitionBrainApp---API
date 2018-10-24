cf247b59927bb1c40f64fe43c0f4125bbec35dc8
Anthony Curtis <tonydc1997@gmail.com>
2018-10-24 06:50:13 -0700

    feat: Check if user has auth. If not, return an error. If so return auth

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -2,6 +2,11 @@ const redis = require('./signIn').redisClient();
 
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers;
-  
+  if (!authorization) {
+    return res.status(401).json('Unauthorized');
+  }
+  return redisClient.get(authorization, (err, reply) => {
+    
+  });
   return next();
 }

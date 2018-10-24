07f7672b60561f97532ec06b70130f4c266ad51e
Anthony Curtis <tonydc1997@gmail.com>
2018-10-24 11:28:02 -0700

    chore: Make spelling consistent across code base

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,11 +1,11 @@
-const redis = require('../controllers/signIn').redisClient;
+const redisClient = require('../controllers/signIn').redisClient;
 
 const requireAuth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json('Unauthorized');
   }
-  return redis.get(authorization, (err, reply) => {
+  return redisClient.get(authorization, (err, reply) => {
     if (err || !reply) {
       return res.status(401).json('Unauthorized');
     }

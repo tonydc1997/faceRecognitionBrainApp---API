bd37585e5fbddda788512285b83e4081c3691a2b
Anthony Curtis <tonydc1997@gmail.com>
2018-10-30 07:05:25 -0700

    feat: Use registerAuthentication on register endpoint

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 
 app.get('/', (req, res) => { res.send('It is working!') })
 app.post('/signIn', (req, res) => { signIn.signInAuthentication(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.registerAuthentication(req, res, db, bcrypt) })
 app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db) })
 app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db) })
 app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db) })

a1716aa2eb664db24a7fc474b0e65bfc89511c63
Anthony Curtis <tonydc1997@gmail.com>
2018-10-30 06:44:12 -0700

    style: Add semicolons, trailing commas, adjust spacing acrding to eslint

--- controllers/register.js
+++ controllers/register.js
@@ -17,18 +17,17 @@ const handleRegister = (req, res, db, bcrypt) => {
           .insert({
             name,
             email: loginEmail[0],
-            joined: new Date()
+            joined: new Date(),
           })
           .then(user => {
             res.json(user[0]);
-          })
+          });
       })
       .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('Sorry! Unable to register.'))
-}
+      .catch(trx.rollback);
+  }).catch(err => res.status(400).json('Sorry! Unable to register.'));
+};
 
 module.exports = {
-  handleRegister
+  handleRegister,
 };

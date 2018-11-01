3b537c0f76ce0155648129b45994241b38bb1d3f
Anthony Curtis <tonydc1997@gmail.com>
2018-11-01 13:44:00 -0700

    style: Address style errors in accordance to eslint rules
    - Fix db.method() alignment within HandleProfileGet.
    
    - Add semicolons to the end of handlers and module.export
    
    - Remove whitespace after a comma within module.exports
    
    - Add newline at end of file
    
    - Use property shorhand within handleProfileUpdate

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,21 +1,23 @@
 const handleProfileGet = (req, res, db) => {
   const { id } = req.params;
-  db.select('*').from('users').where({id})
+  db.select('*')
+    .from('users')
+    .where({ id })
     .then(user => {
       if (user.length) {
         res.json(user[0]);
       } else {
-        res.status(400).json('Sorry! Not found.')
+        res.status(400).json('Sorry! Not found.');
       }
     })
-    .catch(err => res.status(400).json('Sorry! There was a problem getting the user'))
-}
+    .catch(err => res.status(400).json('Sorry! There was a problem getting the user'));
+};
 
 const handleProfileUpdate = (req, res, db) => {
   const { id } = req.params;
   const { name, age, book } = req.body.formInput;
   db('users')
-    .where({ id: id })
+    .where({ id })
     .update({ name, age, book })
     .then(resp => {
       if (resp) {
@@ -25,9 +27,9 @@ const handleProfileUpdate = (req, res, db) => {
       }
     })
     .catch(err => res.status(400).json('Error updating user'));
-}
+};
 
 module.exports = {
-  handleProfileGet, 
-  handleProfileUpdate
-}
+  handleProfileGet,
+  handleProfileUpdate,
+};

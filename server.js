f97400e7a21add383593d0a27ec1c3d8834bd1d4
Anthony Curtis <tonydc1997@gmail.com>
2018-11-01 13:09:25 -0700

    style: Address style issues in server.js according to eslint rules
    - Make REST api adhere to max line-length of 80 characters.
    
    - Add semicolons to end of REST api statements.
    
    - Add semicolons to end of controller handlers statements.

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const db = knex({
   client: 'pg',
   connection: process.env.POSTGRES_URI
   // For Heroku
-    // {
-    // connectionString: process.env.DATABASE_URL,
-    // ssl: true,
+  // {
+  // connectionString: process.env.DATABASE_URL,
+  // ssl: true,
   // }
 });
 
@@ -29,13 +29,27 @@ app.use(bodyParser.json());
 app.use(morgan('combined'));
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('It is working!') })
-app.post('/signIn', (req, res) => { signIn.signInAuthentication(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.registerAuthentication(req, res, db, bcrypt) })
-app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db) })
-app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db) })
-app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageUrl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res) })
+app.get('/', (req, res) => {
+  res.send('It is working!');
+});
+app.post('/signIn', (req, res) => {
+  signIn.signInAuthentication(req, res, db, bcrypt);
+});
+app.post('/register', (req, res) => {
+  register.registerAuthentication(req, res, db, bcrypt);
+});
+app.get('/profile/:id', auth.requireAuth, (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.post('/profile/:id', auth.requireAuth, (req, res) => {
+  profile.handleProfileUpdate(req, res, db);
+});
+app.put('/image', auth.requireAuth, (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageUrl', auth.requireAuth, (req, res) => {
+  image.handleApiCall(req, res);
+});
 app.listen(PORT || 3000, () => {
   console.log(`app is running on ${PORT}`);
 });

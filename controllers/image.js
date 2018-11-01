9075e5a4bb8673cf0846fd9695f6b6fddfff31b2
Anthony Curtis <tonydc1997@gmail.com>
2018-11-01 13:32:35 -0700

    refactor: Move errorResponse in image.js into its own var
    - Replace old code with errorResponse var in handleApiCall .catch()
    
    - Replace old code with errorResponse var in handleImage .catch()

--- controllers/image.js
+++ controllers/image.js
@@ -5,16 +5,22 @@ const app = new Clarifai.App({
 });
 
 const handleApiCall = (req, res) => {
+  const errorResponse = err => {
+    res.status(400).json('Sorry, unable to work with API');
+  };
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('Sorry, unable to work with API'))
+    .catch(errorResponse);
 };
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
+  const errorResponse = err => {
+    res.status(400).json('Unable to get entries');
+  };
   db('users')
     .where('id', '=', id)
     .increment('entries', 1)
@@ -22,7 +28,7 @@ const handleImage = (req, res, db) => {
     .then(entries => {
       res.json(entries[0]);
     })
-    .catch(err => res.status(400).json('Unable to get entries'))
+    .catch(errorResponse);
 };
 
 module.exports = {

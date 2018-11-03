6cad7b307553cad74f6373fb7404040f76a9bd91
Anthony Curtis <tonydc1997@gmail.com>
2018-11-03 07:10:39 -0700

    docs: Change connection string to connnect to correct heroku db

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ const auth = require('./middleware/authorization');
 
 const db = knex({
   client: 'pg',
-  connection: process.env.POSTGRES_URI
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
+  // For local use
+  // process.env.POSTGRES_URI
   // For Heroku
-  // {
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: true,
-  // }
 });
 
 const app = express();

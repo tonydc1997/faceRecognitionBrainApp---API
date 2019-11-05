2fd627d7b3fb0f08f922aaa06365de2ed6a84c66
Anthony Curtis <tonydc1997@gmail.com>
2019-11-05 09:04:48 +0100

    feat: Add optionsSuccessStatus

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
 };
 
 const app = express();

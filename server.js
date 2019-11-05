d1c68014ed718fe3ca36ee443696fa75f649f3f4
Anthony Curtis <tonydc1997@gmail.com>
2019-11-05 09:06:29 +0100

    feat: Make CORS methods explicit
    - Ensure CORS uses get, put, and post requests

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ const corsOptions = {
     }
   },
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
+  methods: ['GET', 'PUT', 'POST'],
 };
 
 const app = express();

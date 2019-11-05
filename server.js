22262250ec04c234dd9cc08cf9846b1b0e523b10
Anthony Curtis <tonydc1997@gmail.com>
2019-11-05 08:23:22 +0100

    feat: Configure Cors for dynamic origin
    - Declare corsOptions w/ 2 props: origin() and optionsSuccessStatus.
    
    - Ensure origin fn() checks domain for white listed urls.
      - Pass or throw new Error based on response.
    
    - Ensure optionsSuccessStatus works on some legacy browsers.

--- server.js
+++ server.js
@@ -33,6 +33,16 @@ const whitelist = [
   'https://app-smartbrain.herokuapp.com',
   'https://powerful-depths-38914.herokuapp.com',
 ];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 const app = express();
 const PORT = process.env.PORT;
